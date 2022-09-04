ae759a44e0a402063ac711400a2f13ef15a4359f
maomao <tangpansak@gmail.com>
2022-09-04 20:55:31 +0700

    Disabling thread with checks

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import typer
 app = typer.Typer()
 
 
+# static methods to get data from system
 def get_current_cpus():
     return int(subprocess.Popen(["nproc"], stdout=subprocess.PIPE).communicate()[0])
 
@@ -36,30 +37,36 @@ def disable_thread(start: int, end: int):
     :param start: starting Thread to disable
     :param end: Ending Thread to disable
     """
-    # check the number of processing units(threads)
-    if start is None or end is None:
-        # this code should be unreachable as typer will error out first
-        print("Start and End cannot be Empty")
-    all_cpu = get_all_cpus()
-    if start == 0:
-        print("You cannot disable Thread 0")
-    elif start > 0 and end <= all_cpu:
-        print(f"Disabling cpu {start} to {end} from total of {all_cpu} Threads")
-        toggle_threads(start, end, 0)
-    elif end > all_cpu:
-        print("You Cannot disable more CPUS than you have")
-    cur_cpu = get_current_cpus()
-    print(f"You now have {cur_cpu}/{all_cpu} CPUs")
+    try:
+        if start is None or end is None:
+            # this code should be unreachable as typer will error out first
+            print("Start and End cannot be Empty")
+        # get the total supported cpus of the installed device
+        all_cpu = get_all_cpus()
+        # prevent the user from killing their own system
+        # it should not be possible to set cpu0 to offline
+        if start == 0:
+            print("You cannot disable Thread 0")
+        # check if the user passed in correct values
+        elif start > 0 and end <= all_cpu:
+            print(f"Disabling cpu {start} to {end} from total of {all_cpu} Threads")
+            # actually start the command
+            if typer.prompt("Are you sure you want to continue? [y/n]").upper() == "Y":
+                toggle_threads(start, end, 0)
+            else:
+                print("Operation Canceled")
+                return
+        # if the user tries to disable more CPUs than they have
+        elif end > all_cpu:
+            print("You cannot disable more CPUS than you have")
+        # sleep to let the system catch up to our changes
+        time.sleep(1)
+        # gets the new CPUs value
+        cur_cpu = get_current_cpus()
+        print(f"You now have {cur_cpu}/{all_cpu} CPUs")
+    except PermissionError:
+        print("You cannot modify your system as a non root account, please re-run this command with sudo")
 
-
-@app.command()
-def enable_thread(start: int = 1, end: int = 1):
-    cur_cpu = get_current_cpus()
-    if start == 0:
-        print("You cannot disable Thread 0")
-    elif start > 0:
-        print(f"Enabling Threads {start} to {end}")
-        toggle_threads(start, end, 1)
 
 
 if __name__ == "__main__":
